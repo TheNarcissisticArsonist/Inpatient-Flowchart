57902b216a8954d8b85c5409613cb85f96e0dbe4
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-27 17:06:41 -0500

    Created and labeled stage variable

---
+++ Flowchart.js
@@ -0,0 +1,10 @@
+var stage = 0;
+/*
+0   -> How does the patient enter the health system?
+1   -> What care level is needed?
+2   -> Treat, discharge, or transfer?
+3.1 -> Treat hub
+3.2 -> Discharged
+3.3 -> Transfer hub
+-1  -> Done
+*/
